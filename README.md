7c17412a08b8bff5f100fffa93aa2c553dc18714
titenq <titenq@gmail.com>
2024-10-16 19:31:50 -0300

    playlist

---
+++ README.md
@@ -0,0 +1,19 @@
+Dependências e Instalação
+FFmpeg:
+
+Instalação: As instruções de instalação do FFmpeg variam de acordo com o sistema operacional. Aqui estão algumas opções comuns:
+Windows: https://www.wikihow.com/Install-FFmpeg-on-Windows
+macOS: Usar Homebrew com o comando brew install ffmpeg.
+Linux: Usar o gerenciador de pacotes da sua distribuição, por exemplo, sudo apt install ffmpeg para distribuições baseadas no Debian/Ubuntu.
+
+Python >= 3.7:
+
+https://www.python.org/downloads
+
+Instalação: A instalação do Python também depende do sistema operacional:
+Windows: Baixar o instalador do Python aqui e garantir que a opção "Add Python to PATH" esteja selecionada durante a instalação.
+macOS: Usar Homebrew com o comando brew install python.
+Linux: Usar o gerenciador de pacotes da sua distribuição, como sudo apt install python3 para distribuições baseadas no Debian/Ubuntu.
+
+Documentação para os Usuários
+Para facilitar para outras pessoas que irão usar seu projeto, você pode incluir um arquivo README.md ou um guia de instalação que explique claramente como instalar o FFmpeg e o Python, junto com os comandos específicos para cada sistema operacional.
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,64 @@
+import { existsSync, mkdirSync } from 'node:fs';
+import { join } from 'node:path';
+
 import progressEstimator from 'progress-estimator';
 import youtubedl from 'youtube-dl-exec';
 
 const logger = progressEstimator();
 
 const downloader = async (url: string) => {
+  let downloadError = 'Erro ao baixar o vídeo:';
+
   try {
-    const metadataPromise = youtubedl(url, { dumpSingleJson: true });
-    const metadata = await logger(metadataPromise, `Obtendo metadata do vídeo ${url}\n`);
+    const outputDir = join(process.cwd(), 'videos');
+
+    if (!existsSync(outputDir)) {
+      mkdirSync(outputDir);
+    }
+
+    const metadataPromise = youtubedl(url, {
+      skipDownload: true,
+      flatPlaylist: true,
+      dumpSingleJson: true
+    });
+    
+    const metadata = await logger(metadataPromise, `Obtendo metadata de ${url}\n`);
     
     if (typeof metadata !== 'string') {
-      const videoTitle = metadata.title.replace(/[<>:"/\\|?*]+/g, '');
-
-      const options = {
+      let options = {
         format: 'bestvideo+bestaudio',
         mergeOutputFormat: 'mp4',
-        output: `./videos/${videoTitle}.mp4`,
+        output: join(outputDir, '%(title)s.%(ext)s'),
         postprocessorArgs: '-movflags faststart'
       };
 
+      let downloadingMessage = `Baixando o vídeo de ${url}\n`;
+      let downloadSuccess = 'Vídeo baixado com sucesso!';
+
+      if (metadata._type === 'playlist') {
+        options = {
+          ...options,
+          output: join(outputDir, '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s')
+        };
+
+        downloadingMessage = `Baixando a playlist de ${url}\n`;
+        downloadSuccess = 'Playlist baixada com sucesso!';
+        downloadError = 'Erro ao baixar a playlist:';
+      }
+
       const promise = youtubedl(url, options);
   
-      await logger(promise, `Baixando o vídeo de ${url}\n`);
+      await logger(promise, downloadingMessage);
       
-      console.log('Vídeo baixado com sucesso!');
+      console.log(downloadSuccess);
 
       return;
     }
 
-    console.log(`Erro ao obter metadata do vídeo ${url}`);
+    console.log(`Erro ao obter metadata ${url}`);
     
   } catch (error) {
-    console.log('Erro ao baixar o vídeo:', error);
+    console.log(downloadError, error);
   }
 };
 
